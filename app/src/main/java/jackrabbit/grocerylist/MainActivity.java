ab3338f7c4aaa28d68f07f3d9c730d71ba2df4db
spaceRaccon <the13fennec@yahoo.com>
2016-03-12 08:35:16 -0500

    Buttons
    added the ability to add items to the list

--- app/src/main/java/jackrabbit/grocerylist/MainActivity.java
+++ app/src/main/java/jackrabbit/grocerylist/MainActivity.java
@@ -10,13 +10,15 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText addTxt;
     private Button addBtn;
     private ListView list;
 
-    String[] values = new String[] {"Apples", "Cookies", "Pizza"};
+    private ArrayList<String> groceryList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +28,16 @@ public class MainActivity extends AppCompatActivity {
         addTxt = (EditText)findViewById(R.id.addView);
 
         addBtn = (Button)findViewById(R.id.addbtn);
+        addBtn.setOnClickListener(this);
 
         list = (ListView)findViewById(R.id.list);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,android.R.layout.simple_expandable_list_item_1, android.R.id.text1, values);
+        // test
+        groceryList.add("pizza");
+        groceryList.add("apples");
+        groceryList.add("soup");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,android.R.layout.simple_expandable_list_item_1, android.R.id.text1, groceryList);
         list.setAdapter(adapter);
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -40,5 +48,17 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Position: " +itemPosition+ " ListItem: " +itemValue, Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String addValue;
+
+        if (v.equals(addBtn)){
+            addValue = addTxt.getText().toString();
+            if (addValue != "")
+                groceryList.add(addValue);
+        }
     }
 }
